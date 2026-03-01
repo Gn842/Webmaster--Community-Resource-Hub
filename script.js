f31067bcdf1af80b662bb35c6ee562ab5ce3d6ec
Gn842 <natoguadalupe9@gmail.com>
2026-03-01 13:14:36 -0500

    Add script to index file for search bar

--- script.js
+++ script.js
@@ -18,15 +18,3 @@ function showSlides (){
 
     setTimeout(showSlides, 4000)
 }
-
-    const searchInput = document.getElementById("searchInput");
-    const itemList = document.getElementById("itemList");
-    const items = itemList.getElementsByTagName("li");
-
-    searchInput.addEventListener("input", function() {
-      const filter = searchInput.value.toLowerCase();
-      for (let i = 0; i < items.length; i++) {
-        const text = items[i].textContent.toLowerCase();
-        items[i].style.display = text.includes(filter) ? "" : "none";
-      }
-    });
